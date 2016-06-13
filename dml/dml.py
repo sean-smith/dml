dcf60f430979cbacff4a6aad89ba26aff09eb8e8
lapets <lapets@gmail.com>
2016-06-13 13:41:17 -0400

    Initial framework files for project.

---
+++ dml/dml.py
@@ -0,0 +1,16 @@
+###############################################################################
+## 
+## dml.py
+##
+##   Python library providing common functionalities for building Data
+##   Mechanics platform components.
+##
+##   Web:     datamechanics.org
+##   Version: 0.0.1.0
+##
+##
+
+###############################################################################
+##
+
+##eof
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name             = 'dml',
+    version          = '0.0.1.0',
+    packages         = ['dml',],
+    install_requires = ['pymongo',],
+    license          = 'MIT License',
+	url              = 'http://datamechanics.org',
+	author           = 'A. Lapets',
+	author_email     = 'a@lapets.io',
+    description      = 'Common functionalities for building Data Mechanics platform components.',
+    long_description = open('README').read(),
+)
